968848e9b3bd2e5863ea8c7cca90e36a8b4e8bfa
Apple@Win11 <1464928815@qq.com>
2025-02-26 22:05:06 +0800

    新增: 计时功能

--- serial/main.c
+++ serial/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "compute.h"
 
 int main()
@@ -13,6 +14,7 @@ int main()
 	float  residual, residual0;
 	float* T = (float*)malloc(sizeof(float) * gridNum);
 	float* TOld = (float*)malloc(sizeof(float) * gridNum);
+	clock_t start, finish;
 
 	if (T == NULL || TOld == NULL) {
 		printf("Memory allocation failed!\n");
@@ -37,20 +39,23 @@ int main()
 	}
 
 	// 开始计算
+	start = clock();
 	for (iter = 1; iter <= maxIter; iter++)
 	{
 		residual = Jacobi(Ni, Nj, T, TOld);
 
 		float* tmp = TOld;
 		TOld = T;
-		//T = tmp;
+		T = tmp;
 
-		if (iter == 1) residual0 = residual;
+		/*if (iter == 1) residual0 = residual;
 		if (iter == 1 || iter % 1000 == 0)
 		{
 			printf("%5d %14.6f\n", iter, residual / residual0);
-		}
+		}*/
 	}
+	finish = clock();
+	printf("\nTime to solution: %.1f [sec]\n", (double)(finish - start) / CLOCKS_PER_SEC);
 
 	// 释放内存
 	free(T);
