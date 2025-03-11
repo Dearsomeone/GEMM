6f6bdb00be3639a0f811755aef38e9ef64b24912
apple@Ubuntu <1464928815@qq.com>
2025-03-11 21:27:00 +0800

    新增: SSE版本的矩阵乘法

---
+++ cmake/simd/sse/compute.h
@@ -0,0 +1,8 @@
+#ifndef COMPUTE_H
+#define COMPUTE_H
+
+/* N * N 方阵乘法 */
+void multiply(int N, float* restrict A, float* restrict B, float* restrict C);
+void printMatrix(int N, float* matrix);
+
+#endif
