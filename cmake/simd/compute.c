f5b72610f7e665a89f0dc021b8470574bb2175ad
apple@Ubuntu <1464928815@qq.com>
2025-03-11 22:10:54 +0800

    改进: 优化simd代码

--- cmake/simd/compute.c
+++ cmake/simd/compute.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include "compute.h"
 
-void multiply(int N, float* A, float* B, float* C)
+void multiply(int N, float* restrict A, float* restrict B, float* restrict C)
 {
 	int i, j, k;
 
-	for (i = 0; i < N; i++) 
+	for (i = 0; i < N; i++)
     {
-        for (j = 0; j < N; j++) 
+        for (k = 0; k < N; k++)
         {
-			C[i * N + j] = 0;
-            for (k = 0; k < N; k++) 
+            for (j = 0; j < N; j++)
             {
                 C[i * N + j] += A[i * N + k] * B[k * N + j];
             }
