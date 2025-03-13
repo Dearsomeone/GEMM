d068592d22a46745a5a345e3afe5a9c3af9c129b
apple@Ubuntu <1464928815@qq.com>
2025-03-13 22:37:37 +0800

    改进: 按ikj顺序求矩阵乘法

---
+++ cmake/serial/optimal/compute.h
@@ -0,0 +1,9 @@
+#ifndef COMPUTE_H
+#define COMPUTE_H
+
+/* N * N 方阵乘法 */
+void multiply(int N, float* A, float* B, float* C);
+void multiplyNew(int N, float* A, float* B, float* C);
+void printMatrix(int N, float* matrix);
+
+#endif
