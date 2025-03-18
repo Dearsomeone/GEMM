2a72a6e78e27cb5a1422acbe4605034a4330f2db
Apple@Win11 <1464928815@qq.com>
2025-03-18 22:06:49 +0800

    改进: 移动路径

---
+++ Linux/cmake/serial/compute.c
@@ -0,0 +1,50 @@
+#include <math.h>
+#include <stdio.h>
+#include "compute.h"
+
+/* ijk遍历，缓存命中率低 */
+void multiply(int N, float* A, float* B, float* C)
+{
+	int i, j, k;
+
+	for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            for (k = 0; k < N; k++)
+            {
+                C[i * N + j] += A[i * N + k] * B[k * N + j];
+            }
+        }
+    }
+}
+
+/* ikj遍历，缓存命中率高 */
+void multiplyNew(int N, float* A, float* B, float* C)
+{
+	int i, j, k;
+
+	for (i = 0; i < N; i++)
+    {
+        for (k = 0; k < N; k++)
+        {
+            for (j = 0; j < N; j++)
+            {
+                C[i * N + j] += A[i * N + k] * B[k * N + j];
+            }
+        }
+    }
+}
+
+void printMatrix(int N, float* matrix)
+{
+	int i, j;
+	for (i = 0; i < N; i++) 
+    {
+        for (j = 0; j < N; j++) 
+        {
+            printf("%8.2f ", matrix[i * N + j]);
+        }
+        printf("\n");
+    }
+}
