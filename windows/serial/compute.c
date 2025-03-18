8cb4265f7a2d903f780b87846159eca86ae7d734
Apple@Win11 <1464928815@qq.com>
2025-03-18 22:01:21 +0800

    改进: 优化程序

--- windows/serial/compute.c
+++ windows/serial/compute.c
@@ -19,23 +19,6 @@ void multiply(int N, float* A, float* B, float* C)
     }
 }
 
-/* ikj遍历，缓存命中率高 */
-void multiplyNew(int N, float* A, float* B, float* C)
-{
-	int i, j, k;
-
-	for (i = 0; i < N; i++)
-    {
-        for (k = 0; k < N; k++)
-        {
-            for (j = 0; j < N; j++)
-            {
-                C[i * N + j] += A[i * N + k] * B[k * N + j];
-            }
-        }
-    }
-}
-
 void printMatrix(int N, float* matrix)
 {
 	int i, j;
