17e74b03017948e8044a440a31462e23d5a1774f
apple@Fedora <1464928815@qq.com>
2025-03-30 17:44:20 +0800

    改进: 串行程序改进

--- linux/serial/compute.c
+++ linux/serial/compute.c
@@ -5,13 +5,11 @@
 /* ijk遍历，缓存命中率低 */
 void multiply(int N, float* A, float* B, float* C)
 {
-	int i, j, k;
-
-	for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (j = 0; j < N; j++)
+        for (int j = 0; j < N; j++)
         {
-            for (k = 0; k < N; k++)
+            for (int k = 0; k < N; k++)
             {
                 C[i * N + j] += A[i * N + k] * B[k * N + j];
             }
@@ -20,11 +18,10 @@ void multiply(int N, float* A, float* B, float* C)
 }
 
 void printMatrix(int N, float* matrix)
-{
-	int i, j;
-	for (i = 0; i < N; i++) 
+{	
+    for (int i = 0; i < N; i++) 
     {
-        for (j = 0; j < N; j++) 
+        for (int j = 0; j < N; j++) 
         {
             printf("%8.2f ", matrix[i * N + j]);
         }
