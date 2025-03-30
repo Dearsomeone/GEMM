9686635587931108b1d333b66709aeb3fd5616e1
apple@Fedora <1464928815@qq.com>
2025-03-30 19:39:54 +0800

    改进: 优化openmp代码

--- linux/openmp/compute.c
+++ linux/openmp/compute.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <omp.h>
 #include "compute.h"
+#include <omp.h>
+#include <stdio.h>
 
 void multiply(int N, float* A, float* B, float* C)
 {
-    #pragma omp parallel for
-    for (int i = 0; i < N; i++) 
-    {
-        for (int k = 0; k < N; k++) 
-        {
-            for (int j = 0; j < N; j++) 
-            {
+#pragma omp parallel for
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
                 C[i * N + j] += A[i * N + k] * B[k * N + j];
             }
         }
@@ -19,11 +16,9 @@ void multiply(int N, float* A, float* B, float* C)
 
 void printMatrix(int N, float* matrix)
 {
-	int i, j;
-	for (i = 0; i < N; i++) 
-    {
-        for (j = 0; j < N; j++) 
-        {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             printf("%8.2f ", matrix[i * N + j]);
         }
         printf("\n");
--- linux/openmp/main.c
+++ linux/openmp/main.c
@@ -1,112 +1,104 @@
+#include "compute.h"
+#include <math.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <math.h>
-#include <omp.h> 
-#include "compute.h"
 
 int main(int argc, char** argv)
 {
-	int N = 1000;
-	unsigned int size = 1000000;
-	int batchSize = 1;
-	int i, j;
-	int numThreads = 28;
-	float* A;
-	float* B;
-	float* C;
-	float* timeData;
-	double timeCost;
-	char* endptr;
-	double start, finish;
-	FILE* file;
-	
-	/* 解析命令行参数 */ 
-	for (i = 1; i < argc; i++) 
-	{
-		switch(i)
-		{
-			case 1:
-				N = (int)strtol(argv[i], &endptr, 10);
-				printf("Command Line Argument %d: %d\n", i, N);
-				break;
-			case 2:
-				batchSize = (int)strtol(argv[i], &endptr, 10);
-				printf("Command Line Argument %d: %d\n", i, batchSize);
-				break;
-			case 3:
-				numThreads = (int)strtol(argv[i], &endptr, 10);
-				printf("Command Line Argument %d: %d\n", i, numThreads);
-				break;
-			default:
-				printf("Warning: Ignore redundant command line arguments!");
-				break;
-		}
+    int N = 1000;
+    unsigned int size = 1000000;
+    int batchSize = 1;
+    int i, j;
+    int numThreads = 28;
+    float* A;
+    float* B;
+    float* C;
+    float* timeData;
+    double timeCost;
+    char* endptr;
+    double start, finish;
+    FILE* file;
+
+    /* 解析命令行参数 */
+    for (i = 1; i < argc; i++) {
+        switch (i) {
+        case 1:
+            N = (int)strtol(argv[i], &endptr, 10);
+            printf("Command Line Argument %d: %d\n", i, N);
+            break;
+        case 2:
+            batchSize = (int)strtol(argv[i], &endptr, 10);
+            printf("Command Line Argument %d: %d\n", i, batchSize);
+            break;
+        case 3:
+            numThreads = (int)strtol(argv[i], &endptr, 10);
+            printf("Command Line Argument %d: %d\n", i, numThreads);
+            break;
+        default:
+            printf("Warning: Ignore redundant command line arguments!");
+            break;
+        }
     }
 
-	omp_set_num_threads(numThreads);
-	/* 测试omp是否开启成功 */
-	#pragma omp parallel for
-	for (int i = 0; i < numThreads; i++) 
-	{
-		printf("Hello World %d from thread = %d\n", i, omp_get_thread_num());
-	}
+    omp_set_num_threads(numThreads);
+/* 测试omp是否开启成功 */
+#pragma omp parallel for
+    for (int i = 0; i < numThreads; i++) {
+        printf("Hello World %d from thread = %d\n", i, omp_get_thread_num());
+    }
 
-	size = N * N;
-	/* 分配内存空间 */
-	A = (float*)malloc(sizeof(float) * size);
-	B = (float*)malloc(sizeof(float) * size);
-	C = (float*)malloc(sizeof(float) * size);
-	timeData = (float*)malloc(sizeof(float) * batchSize);
+    size = N * N;
+    /* 分配内存空间 */
+    A = (float*)malloc(sizeof(float) * size);
+    B = (float*)malloc(sizeof(float) * size);
+    C = (float*)malloc(sizeof(float) * size);
+    timeData = (float*)malloc(sizeof(float) * batchSize);
 
-	if (A == NULL || B == NULL || C == NULL || timeData == NULL) 
-	{
-		printf("Memory allocation failed!\n");
-		return 1;
-	}
+    if (A == NULL || B == NULL || C == NULL || timeData == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
 
-	for (i = 0; i < batchSize; i++)
-	{
-		/* 初始化数组 */
-		memset(C, 0, size * sizeof(float));
-		for (j = 0; j < size; j++)
-		{
-			A[j] = sin(j);
-			B[j] = cos(j);
-		}
+    for (i = 0; i < batchSize; i++) {
+        /* 初始化数组 */
+        memset(C, 0, size * sizeof(float));
+        for (j = 0; j < size; j++) {
+            A[j] = sin(j);
+            B[j] = cos(j);
+        }
 
-		/* 开始计算 */
-		start = omp_get_wtime();
-		multiply(N, A, B, C);
-		finish = omp_get_wtime();
-		timeCost = finish - start;
-		timeData[i] = timeCost;
-		printf("\nTime to solution of the %dth test: %.1f [sec]\n", i, timeCost);
-		/*printMatrix(N, A);
-		printMatrix(N, B);*/
-		// printMatrix(N, C);
-	}
-	
-	/* 计时数据存入文件 */
-	file = fopen("timing.txt", "w");
-    if (file == NULL) 
-	{
+        /* 开始计算 */
+        start = omp_get_wtime();
+        multiply(N, A, B, C);
+        finish = omp_get_wtime();
+        timeCost = finish - start;
+        timeData[i] = timeCost;
+        printf("\nTime to solution of the %dth test: %.1f [sec]\n", i, timeCost);
+        /*printMatrix(N, A);
+        printMatrix(N, B);*/
+        // printMatrix(N, C);
+    }
+
+    /* 计时数据存入文件 */
+    file = fopen("timing.txt", "w");
+    if (file == NULL) {
         perror("Failed to open file");
         return 1;
     }
-    for (i = 0; i < batchSize; i++) 
-	{
+    for (i = 0; i < batchSize; i++) {
         fprintf(file, "%.1f\n", timeData[i]);
     }
     fclose(file);
 
-	/* 释放内存 */
-	free(A);
-	free(B);
-	free(C);
-	free(timeData);
-	A = B = C = timeData = NULL;
+    /* 释放内存 */
+    free(A);
+    free(B);
+    free(C);
+    free(timeData);
+    A = B = C = timeData = NULL;
 
-	return 0;
+    return 0;
 }
