00f9e2c5c0884aec5423548e25bbf599d796b022
apple@Fedora <1464928815@qq.com>
2025-03-30 17:57:02 +0800

    改进: 改进优化后的串行代码

--- linux/serial_optimize/compute.c
+++ linux/serial_optimize/compute.c
@@ -1,18 +1,13 @@
+#include "compute.h"
 #include <math.h>
 #include <stdio.h>
-#include "compute.h"
 
 /* ikj遍历，缓存命中率高 */
 void multiply(int N, float* A, float* B, float* C)
 {
-	int i, j, k;
-
-	for (i = 0; i < N; i++)
-    {
-        for (k = 0; k < N; k++)
-        {
-            for (j = 0; j < N; j++)
-            {
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
                 C[i * N + j] += A[i * N + k] * B[k * N + j];
             }
         }
@@ -21,11 +16,8 @@ void multiply(int N, float* A, float* B, float* C)
 
 void printMatrix(int N, float* matrix)
 {
-	int i, j;
-	for (i = 0; i < N; i++) 
-    {
-        for (j = 0; j < N; j++) 
-        {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             printf("%8.2f ", matrix[i * N + j]);
         }
         printf("\n");
